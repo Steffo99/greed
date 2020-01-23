ab82544c1e1389bfda63d26f9accc27a9fd57485
Darren <darrenwestwood86@gmail.com>
2020-01-23 04:14:40 +0200

    remove test setup api methods

--- blockonomics.py
+++ blockonomics.py
@@ -30,25 +30,3 @@ class Blockonomics:
         else:
           print(r.status_code, r.text)
           return r
-
-    def get_callbacks():
-        api_key = configloader.config["Bitcoin"]["api_key"]
-        url = 'https://www.blockonomics.co/api/address?&no_balance=true&only_xpub=true&get_callback=true'
-        headers = {'Authorization': "Bearer " + api_key}
-        r = requests.get(url, headers=headers)
-        if r.status_code == 200:
-          return r
-        else:
-          return False
-
-    def update_callback(callback_url, xpub):
-        api_key = configloader.config["Bitcoin"]["api_key"]
-        url = 'https://www.blockonomics.co/api/update_callback'
-        headers = {'Authorization': "Bearer " + api_key}
-        data = {'callback':callback_url, 'xpub':xpub}
-        print(data)
-        r = requests.post(url, headers=headers,  json = data)
-        if r.status_code == 200:
-          return r
-        else:
-          print(r.status_code, r.text)
